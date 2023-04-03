bcabaa9c44eef5193e184d3732e2f7b3488bdb46
rcastro999 <113141921+rcastro999@users.noreply.github.com>
2023-04-03 09:08:39 -0600

    cambios en la direccion de la conexion en el archivo de funciones

--- metodos/funciones.php
+++ metodos/funciones.php
@@ -1,7 +1,7 @@
 <?php
 
 function obtener_todos_encabezados_facturas(){
-    include('conexion.php');
+    include('../includes/bd/conexion.php');
     $stmt = $conexion->prepare("SELECT * FROM control_despachos");
     $stmt->execute();
     $resultado = $stmt->fetchAll(); 
