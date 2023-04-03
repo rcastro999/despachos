8913bd920b4f9a565815627d8cbf9b1b14fd1caf
rcastro999 <113141921+rcastro999@users.noreply.github.com>
2023-04-03 10:32:51 -0600

    modificacion de la pantalla del index.php ya que no estaba reconociendo los estilos ni los archivos js

--- includes/header.php
+++ includes/header.php
@@ -5,8 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <!-- Bootstrap CSS -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
 
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php require_once("./includes/header.php"); ?>
     <section class="container stn_principal">
     </section>
+    <?php include("./includes/links_footer.php"); ?>
 <?php require_once("./includes/footer.php"); ?>
